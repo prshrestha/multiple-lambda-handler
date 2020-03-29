5ad965ceef6f4063758230ef64902589f85c9ca8
Prashanta <pshrestha6@gatech.edu>
2020-03-28 21:42:08 -0400

    .

--- src/main/java/handler/first/FirstHandler.java
+++ src/main/java/handler/first/FirstHandler.java
@@ -3,9 +3,9 @@ package handler.first;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class HandlerOne implements RequestHandler<String, String> {
+public class FirstHandler implements RequestHandler<String, String> {
 
     public String handleRequest(String input, Context context) {
-        return "Hello from lambda 1 + " + input;
+        return "Hello from handler 1 + " + input;
     }
 }
--- src/main/java/handler/second/SecondHandler.java
+++ src/main/java/handler/second/SecondHandler.java
@@ -4,9 +4,9 @@ package handler.second;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class HandlerTwo implements RequestHandler<String, String> {
+public class SecondHandler implements RequestHandler<String, String> {
 
     public String handleRequest(String input, Context context) {
-        return "Hello from lambda 2 + " + input;
+        return "Hello from handler 2 + " + input;
     }
 }
